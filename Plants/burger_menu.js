4fd4ef738933ab9f34cb3ac1a29c96e0c17fee66
Margarita Masko <margaritam996@gmail.com>
2023-08-14 12:36:36 +0300

    feat: add smooth scroll to burger-menu

--- Plants/burger_menu.js
+++ Plants/burger_menu.js
@@ -7,14 +7,27 @@ if(menu && navigation){
     menu.addEventListener('click',(e)=>{
       menu.classList.toggle('active');
       navigation.classList.toggle('active');
-      body.classList.toggle('lock');
+      body.classList.add('lock');
 })  
   navigation.addEventListener('click', ()=>{
   if(menu.classList.contains('active')) {
       menu.classList.remove('active');
       navigation.classList.remove('active');
+      body.classList.remove('lock');
   }})
 }
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+    anchors.forEach(anchor => {
+        anchor.addEventListener('click',event =>{
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href').substring(1);
+            document.getElementById(blockID).scrollIntoView({
+                behavior:'smooth',
+                block:'start'
+            })
+        })
+    })
 }
 
 export {burgerMenu}
