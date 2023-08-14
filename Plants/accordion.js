41c345f1cb5483d9ec36682a67b5213ef7bc6097
Margarita Masko <margaritam996@gmail.com>
2023-08-14 18:46:10 +0300

    feat: add accordion space beetween buttons

--- Plants/accordion.js
+++ Plants/accordion.js
@@ -1,9 +1,11 @@
 const accordions = document.querySelectorAll(".accordion_choice");
 const pricesAccordion = document.querySelector(".prices__accordion");
+const offers = document.querySelectorAll(".accordion");
+
 function showOffer(){
     accordions.forEach(accordion=>{
         accordion.addEventListener("click",()=>{
-
+           
             if(accordion.classList.contains('active')){
              accordion.classList.remove('active');
              pricesAccordion.classList.remove('active');
@@ -15,9 +17,17 @@ function showOffer(){
                 })
                 accordion.classList.add('active');
                 pricesAccordion.classList.add('active');
-            }
+        }
+        
+            if(accordions[0].classList.contains('active')){
+                offers[1].classList.add('open');
+            }else{offers[1].classList.remove('open');}
 
-           
+            if(accordions[1].classList.contains('active')){
+                offers[2].classList.add('open');
+            }else {
+                offers[2].classList.remove('open');
+            }
         })
     })
 }
